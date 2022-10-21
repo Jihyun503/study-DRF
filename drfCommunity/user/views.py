62a0c292896b246c1f1aa8e0aa3b56e15e5d9486
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-10-21 18:11:47 +0900

    user ListCreateAPIView 생성

--- drfCommunity/user/views.py
+++ drfCommunity/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from user.models import User
+from user.serializers import UserSerializer
+
+class UserListCreateView(ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
