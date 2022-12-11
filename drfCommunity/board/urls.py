77fa2d086074eb151106ff427299e4b7d86992d0
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-12-11 16:49:27 +0900

    viewset, router

--- drfCommunity/board/urls.py
+++ drfCommunity/board/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from board.views import PostListCreateView
+from board.views import PostListCreateView, PostDetailView, PostViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+# 첫 번째 인자는 url의 prefix
+# 두 번째 인자는 ViewSet
+router.register('post', PostViewSet)
 
 urlpatterns = [
     path('list/', PostListCreateView.as_view()),
+    path('<int:pk>/', PostDetailView.as_view()),
+    path('', include(router.urls))
 ]
--- drfCommunity/board/views.py
+++ drfCommunity/board/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from board.models import Post
 from board.serializers import PostSerializer
+from rest_framework import viewsets
 
 class PostListCreateView(ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+class PostDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
