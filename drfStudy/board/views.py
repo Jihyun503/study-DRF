b1a28f7372f3b034259f4f8de52ae9c300ee805c
Jihyun503 <s2017s17@e-mirim.hs.kr>
2022-12-13 22:07:27 +0900

    authentication 과 permissions 적용

--- drfStudy/board/views.py
+++ drfStudy/board/views.py
@@ -3,10 +3,15 @@ from .models import Board
 from .serializers import BoardSerializer
 from rest_framework.generics import ListCreateAPIView
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 
 class BoardViewSet(viewsets.ModelViewSet):
     serializer_class = BoardSerializer
     queryset = Board.objects.all()
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
